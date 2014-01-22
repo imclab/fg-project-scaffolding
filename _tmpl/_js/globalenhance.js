893773a74f0f3fca3db731584c1bbd03a5cb5974
Jeffrey Lembeck <jlembeck@gmail.com>
2014-01-22 09:57:22 -0800

    Fire enhance on domready

--- _tmpl/_js/globalenhance.js
+++ _tmpl/_js/globalenhance.js
@@ -2,9 +2,6 @@
 // Many plugins bind to an "enhance" event to init themselves on dom ready, or when new markup is inserted into the DOM
 (function( $ ){
 	$( function(){
-		$( ".page:not(.page-hidden)", document ).trigger( "enhance" );
-		setTimeout(function(){
-			$( document.documentElement ).removeClass( "enhancing" );
-		}, 0);
+		$( document ).trigger( "enhance" );
 	});
 }( jQuery ));
